1e1b12b6a357c9f2d3f91d476870e07cd2c29294
Natali Ustinova <nata@purple.studio>
2022-11-03 21:12:08 +0200

    feat: card routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 
@@ -12,16 +13,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', () => console.log('connected'));
+//временная авторизация
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6363fc42ac871e993a20a373'
+  };
+
+  next();
+});
 
 app.use('/users', userRouter);
+app.use('/cards', cardRouter);
 
-//временная авторизация
-// app.use((req, res, next) => {
-//   req.user = {
-//     _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
-//   };
-//
-//   next();
-// });
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
