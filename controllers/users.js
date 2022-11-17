2214ef994b27bfde73af2742c44403dc06e717f5
Natali Ustinova <nata@purple.studio>
2022-11-17 21:40:33 +0200

    feat: JWT in prod

--- controllers/users.js
+++ controllers/users.js
@@ -7,6 +7,7 @@ const EmailExistError = require('../errors/email-exist-err');
 const {
   ERROR_MESSAGE, ERROR_NAME,
 } = require('../constants/constants');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports.getAllUsers = (req, res, next) => {
   User.find({})
@@ -119,7 +120,7 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'strongest-key-ever', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'strongest-key-ever', { expiresIn: '7d' });
       res.send({ token });
     })
     .catch(next);
