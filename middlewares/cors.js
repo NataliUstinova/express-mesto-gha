3d31191bf53cf7abb5993f83779f2a4f4eb1c13f
Natali Ustinova <nata@purple.studio>
2022-11-18 11:56:09 +0200

    fix: CORS 6.3

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -7,7 +7,7 @@ const allowedCors = [
   'http://api.mesto.nata.u.nomoredomains.icu',
 ];
 
-module.exports.cors = (req, res, next) => {
+module.exports = (req, res, next) => {
   console.log('Cors is used');
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   // сохраняем список заголовков исходного запроса
