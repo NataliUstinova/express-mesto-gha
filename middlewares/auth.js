53e2ff9892f70ceb582d248bfd3597fd92eb34f1
Natali Ustinova <nata@purple.studio>
2022-11-18 17:26:17 +0200

    fix: cookie parser

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,22 @@
 const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/auth-err');
+const cookieParser = require('cookie-parser');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 // eslint-disable-next-line consistent-return
 module.exports.auth = (req, res, next) => {
   // Получаем токен из cookies
+  cookieParser()
+  console.log(req.cookie)
+  console.log(req.cookies)
+  // Cookies that have not been signed
+  console.log('Cookies: ', req.cookies)
+
+  // Cookies that have been signed
+  console.log('Signed Cookies: ', req.signedCookies)
   const token = req.cookies.jwt;
+
   let payload;
 
   // Чтобы отловить ошибки оборачиваем в try-catch
